c6cf548437241d8eaeb4adea23db9cb0810a5b53
CoRu.ws <vsa88662@haqed.com>
2016-07-27 08:29:41 +0300

    Update sf.py

--- sf.py
+++ sf.py
@@ -17,6 +17,7 @@ import os
 import sys
 import pefile
 import struct
+import argparse
 
 #------------------------------------------------
 # common functions
@@ -29,7 +30,7 @@ def SetHome(path):
 	home = os.path.realpath(os.path.dirname(path))
 	os.chdir(home)
 	
-def ReadFile(path):
+def get_file_data(path):
 	if os.path.isfile(path):
 		f = open(path, 'rb')	
 		data = f.read()
@@ -43,11 +44,13 @@ def SaveFile(path,data):
 	f.write(data)
 	f.close()
 		
-def CreateOutputFolder(path):
+def get_output_dir(path,mode,opt=None):
 	name = os.path.basename(path)
 	tmp = name.split('.')
 	name = tmp[0]
-	dir_name = name+'_SignFinder'
+	dir_name = "{} [{}]".format(name,mode.__name__)
+	if opt is not None:
+		dir_name = "{} [{}] [{}]".format(name,mode.__name__,opt)		
 	if not os.path.isdir(dir_name):
 		os.mkdir(dir_name)
 	return dir_name
@@ -115,90 +118,135 @@ def pefile_StructToOffsets(structure):
 	offset_list = (structure[0], tmp_list)
 	return offset_list
 		
-# get interesting info from pefile
-def GetPeInfo(file_data):
-	try:
-		pe =  pefile.PE(data=file_data, fast_load = True)
-		info = dict() 
-		section_list = list()
-		section_num = 0
-		
-		for section in pe.sections:
-			name = section.Name.replace('\x00','')
-			name = '[{}]{}'.format(section_num,name)
-			section_tmp = {'name':name, 'offset':section.PointerToRawData, 'size':section.SizeOfRawData}
-			section_list.append(section_tmp)
-			section_num += 1
-		
-		info['sections'] = section_list
+def get_section_info(pe): 
+	section_list = list()
+	section_num = 0
+	for section in pe.sections:
+		name = section.Name.replace('\x00','')
+		name = '[{}]{}'.format(section_num,name)
+		section_tmp = {'name':name, 'offset':section.PointerToRawData, 'size':section.SizeOfRawData}
+		section_list.append(section_tmp)
+		section_num += 1
+	return section_list
 
-		import_offset = pe.get_offset_from_rva(pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress)
-		import_dir = (import_offset, pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].Size)
-		info['import'] = import_dir
-		
-		ep_offset = pe.get_offset_from_rva(pe.OPTIONAL_HEADER.AddressOfEntryPoint)
-		info['ep'] = ep_offset
-		
-		info['optional_offset'] = pe.OPTIONAL_HEADER.get_file_offset()
-		
-		return info
+def get_headers_info(pe): 
+	info = {}
+	ep_offset = pe.get_offset_from_rva(pe.OPTIONAL_HEADER.AddressOfEntryPoint)
+	info['ep'] = ep_offset
+	info['optional_offset'] = pe.OPTIONAL_HEADER.get_file_offset()
+	return info
 
-	except Exception, e:
-		DieWithError(str(e))
+def get_directories_info(pe): 
+	info = {}
+	import_offset = pe.get_offset_from_rva(pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress)
+	import_dir = (import_offset, pe.OPTIONAL_HEADER.DATA_DIRECTORY[1].Size)
+	info['import'] = import_dir
+		
+	import_offset = pe.get_offset_from_rva(pe.OPTIONAL_HEADER.DATA_DIRECTORY[2].VirtualAddress)
+	import_dir = (import_offset, pe.OPTIONAL_HEADER.DATA_DIRECTORY[2].Size)
+	info['resource'] = import_dir
+	
+	import_offset = pe.get_offset_from_rva(pe.OPTIONAL_HEADER.DATA_DIRECTORY[5].VirtualAddress)
+	import_dir = (import_offset, pe.OPTIONAL_HEADER.DATA_DIRECTORY[5].Size)
+	info['reloc'] = import_dir
+	return info
+	
+#need PE(fast_load=False)
+def get_iat_info(pe): 
+	info = {}
+	info['lib'] = list()
+	info['iat'] = list()
+	for entry in pe.DIRECTORY_ENTRY_IMPORT:
+		dll = entry.dll
+		
+		if dll not in info['lib']:
+			info['lib'].append(dll)
+		
+		for imp in entry.imports:
+			iat = dict() 
+			iat['addr'] = hex(imp.address)
+			iat['name_offset'] = hex(imp.name_offset)
+			iat['name'] = imp.name
+			iat['dll'] = dll
+			info['iat'].append(iat)	
+	return info
 
-			
 #------------------------------------------------
-# work modes
+# working modules
 #------------------------------------------------
 
-def mode_fast(file_data, dir_name, pe_info):
-
+def mode_fast(param,args):
+	dir_name = get_output_dir(param['path'],mode_fast)
+	file_data = param['data']
+	
 	# replace code on entry point 
+	headers = get_headers_info(param['pe'])
 	new_byte = '\xCC\xC3' # INT3 RET
-	new_data = ReplaceByteString(file_data,pe_info['ep'],new_byte)
+	new_data = ReplaceByteString(file_data,headers['ep'],new_byte)
 	SaveClean('EMUL', dir_name, new_data)
 	
 	# delete import
-	new_data = ReplaceByte(file_data, pe_info['import'][0], pe_info['import'][1], '\x00')
+	dir = get_directories_info(param['pe'])
+	new_data = ReplaceByte(file_data, dir['import'][0], dir['import'][1], '\x00')
 	SaveClean('IMPORT', dir_name, new_data)
 	
+	sections = get_section_info(param['pe'])
+	
 	# clean sections data one by one
-	for sect in pe_info['sections']:
+	for sect in sections:
 		new_data = ReplaceByte(file_data, sect['offset'], sect['size'], '\x00')
 		SaveClean('SECTION{}'.format(sect['name']), dir_name, new_data)
 		
 	# clean ALL sections
 	new_data = file_data
-	for sect in pe_info['sections']:
+	for sect in sections:
 		new_data = ReplaceByte(new_data, sect['offset'], sect['size'], '\x00')
 	SaveClean('ALL_SECTION', dir_name, new_data)
 		
 	# cleanse all sections but one
-	for sect in pe_info['sections']:
+	for sect in sections:
 		new_data = file_data
-		for sect2 in pe_info['sections']:
+		for sect2 in sections:
 			if sect2['name'] != sect['name']:
 				new_data = ReplaceByte(new_data, sect2['offset'], sect2['size'], '\x00')
 		SaveClean('ALL_SECTION_NOT{}'.format(sect['name']), dir_name, new_data)
 		
 	print "[-] Fast mode - done" 
 
+#------------------------------------------------
+#show info about pe struct
+def mode_info(param,args):
+	str = ''
+	for s in get_section_info(param['pe']):
+		str += '{}\t{}\t{}\n'.format(s['name'], s['offset'], s['size'])
+	print (str)
 
-def mode_header(file_data, dir_name, pe_info):
+#------------------------------------------------	
+def mode_header(param,args):
+	dir_name = get_output_dir(param['path'],mode_header)
+	file_data = param['data']
 	# start from DosHeader
 	CleanHeaderStruct(file_data, 0, dir_name, pefile.PE.__IMAGE_DOS_HEADER_format__)
 	# then OptionalHeader
-	CleanHeaderStruct(file_data, pe_info['optional_offset'], dir_name, pefile.PE.__IMAGE_OPTIONAL_HEADER_format__)
+	headers = get_headers_info(param['pe'])
+	CleanHeaderStruct(file_data, headers['optional_offset'], dir_name, pefile.PE.__IMAGE_OPTIONAL_HEADER_format__)
 	print "[-] Header mode - done" 
-	
 
+#------------------------------------------------
 # divide the section into 100 pieces and cut them one by one
-def mode_section(file_data, dir_name, pe_info, sect_num):
+def mode_section(param,args):
+	sect_num  = args.section_number
+	sections  = get_section_info(param['pe'])
+	file_data = param['data']
+	part_num = args.p
+
+	opt = "{}-{}".format(sect_num,part_num)
+	dir_name  = get_output_dir(param['path'],mode_section,opt)
+		
 	found = False
 	sect_i = 0 
-	for sect in pe_info['sections']:
+	for sect in sections:
 		if sect_num == sect_i:
-			part_num = 100
 			part_size = sect['size'] / part_num
 			
 			last_part_size = sect['size'] % part_num
@@ -222,12 +270,18 @@ def mode_section(file_data, dir_name, pe_info, sect_num):
 	else:
 		DieWithError('invalid section number')
 
-
-def mode_manual(file_data, dir_name, file_offset, offset_size, part_num):
-
+#------------------------------------------------
+def mode_manual(param,args):
+	offset  = args.offset
+	offset_size  = args.size
+	part_num  = args.part_number
+	file_data = param['data']
+	
+	opt = "{}-{}-{}".format(offset,offset_size,part_num)
+	dir_name  = get_output_dir(param['path'],mode_manual,opt)
+	
 	part_size = offset_size / part_num
 	last_part_size = offset_size % part_num
-	offset = file_offset
 	
 	for i in range(part_num):
 		file_name = 'MANUAL_{}_PART-{}-{}'.format( i, offset, part_size)
@@ -239,11 +293,17 @@ def mode_manual(file_data, dir_name, file_offset, offset_size, part_num):
 		CleanFileOffset(file_data, dir_name, offset, last_part_size, file_name)
 		
 	print "[-] Manual mode - done"  
+ 
+#------------------------------------------------
+def mode_manual2(param,args):
+	offset  = args.offset
+	offset_size  = args.size
+	window_size  = args.window_size
+	file_data = param['data']
 	
+	opt = "{}-{}-{}".format(offset,offset_size,window_size)
+	dir_name  = get_output_dir(param['path'],mode_manual2,opt)
 	
-def mode_manual2(file_data, dir_name, file_offset, offset_size, window_size):
-
-	offset = file_offset
 	max_offset = offset + offset_size - window_size
 	i = 0
 	while 1:
@@ -253,76 +313,65 @@ def mode_manual2(file_data, dir_name, file_offset, offset_size, window_size):
 		CleanFileOffset(file_data, dir_name, offset, window_size, file_name)
 		offset += 1
 		i += 1
-	print "[-] Manual2 mode - done"   
-	
+	print "[-] Manual2 mode - done"  
 
-#show info about pe struct
-def mode_info(pe_info):
-	str = ''
-	for s in pe_info['sections']:
-		str += '{}\t{}\t{}\n'.format(s['name'], s['offset'], s['size'])
-	print (str)
-	
 #------------------------------------------------
 # control functions
 #------------------------------------------------
-
+	
 def main():
 	PrintLogo()
 
-	if len(sys.argv)>=3:
-		mode = sys.argv[1]
-		file_path = sys.argv[2]
-		
-		SetHome(file_path)
-		dir_name = CreateOutputFolder(file_path)
-		file_data = ReadFile(file_path)
-		pe_info = GetPeInfo(file_data)
-		
-		if mode=='fast':
-			mode_fast(file_data, dir_name, pe_info)
-
-		elif mode=='info':
-			mode_info(pe_info)
-
-		elif mode=='head':
-			mode_header(file_data, dir_name, pe_info)			
+	parser = argparse.ArgumentParser()
+	parser.add_argument("path")
+	subparsers = parser.add_subparsers()
 	
-		elif mode=='sect':
-			if len(sys.argv)==4:
-				sect_num = int(sys.argv[3])
-				mode_section(file_data, dir_name, pe_info, sect_num)
-			else:
-				DieWithError('lost sect param')
-		elif mode=='man':
-			if len(sys.argv)==6:
-				file_offset = int(sys.argv[3])
-				offset_size = int(sys.argv[4])
-				part_num    = int(sys.argv[5])
-				mode_manual(file_data, dir_name, file_offset, offset_size, part_num)
-			else:
-				DieWithError('lost man param')
-				
-		elif mode=='man2':
-			if len(sys.argv)==6:
-				file_offset = int(sys.argv[3])
-				offset_size = int(sys.argv[4])
-				window_size    = int(sys.argv[5])
-				mode_manual2(file_data, dir_name, file_offset, offset_size, window_size)
-			else:
-				DieWithError('lost man param')			
-		else:
-			DieWithError('invalid mode')
-	else:
-		print """
-USAGE:	SF.py mode param"
-EXAMPLE:
-	SF.py fast path_to_exe 
-	SF.py head path_to_exe 
-	SF.py sect path_to_exe section_number
-	SF.py man path_to_exe offset size part_num
-	SF.py man2 path_to_exe offset size window_size
-	SF.py info path_to_exe"""
-
+	parser_info = subparsers.add_parser('info', help='section offset and size')
+	parser_info.set_defaults(func=mode_info)
+	
+	parser_fast = subparsers.add_parser('fast', help='localization signature place')
+	parser_fast.set_defaults(func=mode_fast)
+		
+	parser_head = subparsers.add_parser('head', help='test Optional/Dos Headers')
+	parser_head.set_defaults(func=mode_header)
+	
+	parser_sect = subparsers.add_parser('sect', help='search signatures in section')
+	parser_sect.set_defaults(func=mode_section,p=100)
+	parser_sect.add_argument("section_number", type=int)
+	parser_sect.add_argument("-p", type=int, help='part_number')
+		
+	parser_man  = subparsers.add_parser('man', help='manual mode')
+	parser_man.set_defaults(func=mode_manual)
+	parser_man.add_argument("offset", type=int)
+	parser_man.add_argument("size", type=int)
+	parser_man.add_argument("part_number", type=int)
+	
+	parser_man2 = subparsers.add_parser('man2', help='manual mode two')
+	parser_man2.set_defaults(func=mode_manual2)
+	parser_man2.add_argument("offset", type=int)
+	parser_man2.add_argument("size", type=int)
+	parser_man2.add_argument("window_size", type=int)	
+	
+	
+	args = parser.parse_args()
+	path = args.path
+	
+	if not os.path.isfile(path):
+		DieWithError("File not found")
+	data = get_file_data(path)
+		
+	try:
+		pe_info = pefile.PE(data = data, fast_load=True)
+	except pefile.PEFormatError, e:
+		DieWithError("PEFormatError:" + e)
+	
+	SetHome(path)
+	param = {}
+	param['path'] = path
+	param['data'] = data
+	param['pe'] = pe_info
+	args.func(param,args)
+	sys.exit()
+	
 if __name__ == '__main__':
 	main()
